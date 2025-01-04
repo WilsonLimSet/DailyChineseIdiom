57a12ee3d230320a5aa8e4c5f7212a9176b08606
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2025-01-04 15:15:30 -0800

    Initial Commit

---
+++ DailyChineseIdiom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyChineseIdiom
+//
+//  Created by Wilson Lim Setiawan on 1/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyChineseIdiom/DailyChineseIdiomApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyChineseIdiomApp.swift
+//  DailyChineseIdiom
+//
+//  Created by Wilson Lim Setiawan on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyChineseIdiomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
